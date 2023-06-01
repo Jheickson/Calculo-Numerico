09981d7dec5edfc207efb50076a92734f89c69b3
Jheickson <felipesantosatm@outlook.com>
2023-06-01 12:08:08 -0300

    Update IntNum.java

--- Integraçao Numerica/intNum.java
+++ Integraçao Numerica/intNum.java
@@ -9,19 +9,19 @@ public class intNum {
       double[] f;
       
       for (int i = 1 ; i <= n ; i++ ){
-          
+
       int h = Math.abs( (int)x1 - (int)x2);
       
       f[i] = (double) (1 / (Math.pow(x1, 2)));
       f[i+1] = (double) (1 / (Math.pow(x2, 2)));
-      
+
       double A = (h/n) * (f[1] + f[2]);
 
       System.out.println(A);
       System.out.println(A - 3.0612);
       }
-      
 
+      System.out.println("Teste")
 
     }
 }
